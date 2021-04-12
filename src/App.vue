735a49ce68b778787c3a8405839fbfa81ae3147c
Loc Nguyen Vu <ngynvuloc@gmail.com>
2021-04-12 22:27:14 +0700

    Remove about page & fix logout function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light" v-if="isClientError == false">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <router-link to="/" class="navbar-brand">Hola</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -50,9 +50,8 @@ export default {
   },
   methods: {
     logout() {
-      this.$store.commit('logout')
+      this.$store.dispatch('logout')
       this.$store.commit('setClientError', 401)
-      this.$router.push('/error')
     }
   }
 }
