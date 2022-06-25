bb400eb4a68ae57c4e151008cd558aff6a2a675b
locnguyenvu <ngynvuloc@gmail.com>
2022-06-25 16:02:20 +0700

    Chart watch monthly expense

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,9 @@
       <expense-by-category-doughnut-chart-card />
     </div>
     <div class="col-sm-12 col-md-4 col-lg-4">
+      <watch-monthly-expense-line-chart-card />
+    </div>
+    <div class="col-sm-12 col-md-4 col-lg-4">
       <expense-by-month-line-chart-card />
     </div>
   </div>
@@ -13,12 +16,14 @@
 // @ is an alias to /src
 import ExpenseByCategoryDoughnutChartCard from '@/components/Dashboard/ExpenseByCategoryDoughnutChartCard.vue'
 import ExpenseByMonthLineChartCard from '@/components/Dashboard/ExpenseByMonthLineChartCard.vue'
+import WatchMonthlyExpenseLineChartCard from '@/components/Dashboard/WatchMonthlyExpenseLineChartCard.vue'
 
 export default {
   name: 'Home',
   components: {
     ExpenseByCategoryDoughnutChartCard,
-    ExpenseByMonthLineChartCard
+    ExpenseByMonthLineChartCard,
+		WatchMonthlyExpenseLineChartCard,
   }
 }
 </script>
